dd616762590d80ea4b8ce91698295decd1bf2ff1
jonanderson0 <71406833+jonanderson0@users.noreply.github.com>
2020-10-08 12:58:19 -0700

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -8,9 +8,11 @@ Return the missing side
 (2 points)
 """
 def hypotenuse(a,b,c): 
-  return sum(a,b,c)
- x = hypotenuse(3,4,False) == 5
- print(x)
- y = hypotenuse(13,5,True) == 12
- print(y)
-  
+  if c ==True:
+    return math.hypot(a,b)
+  elif c = False:
+    math.sqrt
+    
+
+    
+    
