09d67b2065316045256aba6cca74f9a1963dc15b
jonanderson0 <71406833+jonanderson0@users.noreply.github.com>
2020-10-08 11:49:46 -0700

    Update task4.py

--- task4.py
+++ task4.py
@@ -6,4 +6,12 @@ Return True if the number is an integer
 Return False if the number is not an integer
 (2 points)
 """
+def isInteger(I):
+  return"." not in str(I)
 
+x = isInteger(I):
+  print(x)
+y = isInteger(-2)
+  print(y)
+  
+  
